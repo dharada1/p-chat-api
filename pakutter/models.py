8fb21f5e2d7701847505f6b941936116cc4c8a59
Daichi Harada <haradadaichi715@gmail.com>
2017-09-07 12:41:34 +0900

    Tweetモデル作成

--- pakutter/models.py
+++ pakutter/models.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Tweet(models.Model):
+    text = models.CharField(max_length=140)
+    user_id = models.IntegerField(default=0)
