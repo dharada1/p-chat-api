e276027b3fb1b7ad54ee46d695ba50c32564751c
Daichi Harada <haradadaichi715@gmail.com>
2017-09-07 14:35:54 +0900

    /pakutter/ インデックスでツイートを表示

--- pakutter/urls.py
+++ pakutter/urls.py
@@ -4,5 +4,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /pakutter/
     url(r'^$', views.index, name='index'),
+    # ex: /pakutter/user/1/
+    url(r'^user/(?P<user_id>[0-9]+)/$', views.user, name='user'),
+    # ex: /pakutter/tweet/1/
+    url(r'^tweet/(?P<tweet_id>[0-9]+)/$', views.tweet, name='tweet'),
 ]
--- pakutter/views.py
+++ pakutter/views.py
@@ -3,7 +3,24 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Tweet
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    tweet_list = Tweet.objects.order_by('id')
+    template = loader.get_template('index.html')
+    context = {
+        'tweet_list': tweet_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def user(request, user_id):
+    # TODO
+    response = "ユーザーID%s"
+    return HttpResponse(response % user_id)
+
+def tweet(request, tweet_id):
+    #TODO
+    response = "ツイートID%s"
+    return HttpResponse(response % tweet_id)
