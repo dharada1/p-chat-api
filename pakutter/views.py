febaf09a475f355d9987df5e900bd89747bb9dec
Daichi Harada <haradadaichi715@gmail.com>
2017-09-07 15:55:15 +0900

    ログイン処理

--- pakutter/views.py
+++ pakutter/views.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 
 from .models import Tweet
 
+@login_required
 def index(request):
     tweet_list = Tweet.objects.order_by('id')
     template = loader.get_template('index.html')
@@ -15,11 +17,13 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+@login_required
 def user(request, user_id):
     # TODO
     response = "ユーザーID%s"
     return HttpResponse(response % user_id)
 
+@login_required
 def tweet(request, tweet_id):
     #TODO
     response = "ツイートID%s"
