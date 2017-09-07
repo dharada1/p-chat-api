02235aee4865cccadba6e2aa7edc277ba48f9d55
Daichi Harada <haradadaichi715@gmail.com>
2017-09-07 21:41:11 +0900

    ツイートにcreated_atカラムを追加 表示を新しい投稿から順に表示していく。

--- pakutter/models.py
+++ pakutter/models.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 class Tweet(models.Model):
     text = models.CharField(max_length=140)
     user_id = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
