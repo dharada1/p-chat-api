9423b43eb1d51afec7049c1e615693c4814fc78c
入江 政行 <iriemasayuki@irienoMacBook-Air.local>
2017-09-07 16:27:20 +0900

    ツイート機能途中

--- pakutter/views.py
+++ pakutter/views.py
@@ -7,6 +7,8 @@ from django.template import loader
 
 from .models import Tweet
 
+from .forms import MyForm
+
 def index(request):
     tweet_list = Tweet.objects.order_by('id')
     template = loader.get_template('index.html')
@@ -24,3 +26,12 @@ def tweet(request, tweet_id):
     #TODO
     response = "ツイートID%s"
     return HttpResponse(response % tweet_id)
+
+def form_post(request):
+    if request.method == "POST":
+        form = MyForm(data=request.POST)  # ← 受け取ったPOSTデータを渡す
+        if form.is_valid():  # ← 受け取ったデータの正当性確認
+            pass  # ← 正しいデータを受け取った場合の処理
+    else:  # ← methodが'POST'ではない = 最初のページ表示時の処理
+        form = MyForm()
+    return redirect('index')
