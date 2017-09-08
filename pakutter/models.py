d9cbe3cb7a4327a69f856972018b94c8a1c201ab
Daichi Harada <haradadaichi715@gmail.com>
2017-09-08 12:15:45 +0900

    FollowテーブルでForeignKeyをつかうよう変更

--- pakutter/models.py
+++ pakutter/models.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
+
+class User(DjangoUser):
+    # 新規にtableを作成せずに継承したmodelの拡張のみを行う
+    class Meta:
+        proxy = True
+
+    # 自分がフォローしてる人たち
+    def followees(self):
+        follows = Follow.objects.filter(from_user=self, deleted=False)
+        return [follow.to_user for follow in follows]
+
+    # 自分をフォローしてくれてる人たち
+    def followers(self):
+        follows = Follow.objects.filter(to_user=self, deleted=False)
+        return [follow.from_user for follow in follows]
 
 class Tweet(models.Model):
     text = models.CharField(max_length=140)
@@ -8,25 +24,7 @@ class Tweet(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Follow(models.Model):
-    from_id = models.IntegerField(default=0)
-    to_id = models.IntegerField(default=0)
+    from_user = models.ForeignKey(User, related_name='from_user', null=True)
+    to_user = models.ForeignKey(User, related_name='to_user', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     deleted = models.BooleanField(default=False)
-
-    # フォロイー(フォローしてる人)のid_listを返す。
-    @classmethod
-    def followee_id_list(self, user_id):
-      follow_list = Follow.objects.filter(from_id=user_id, deleted=False)
-      id_list = []
-      for follow in follow_list:
-        id_list.append(follow.to_id)
-      return id_list
-
-    # フォロワー(フォローされてる人)のid_listを返す
-    @classmethod
-    def follower_id_list(self, user_id):
-      follow_list = Follow.objects.filter(to_id=user_id, deleted=False)
-      id_list = []
-      for follow in follow_list:
-        id_list.append(follow.from_id)
-      return id_list
