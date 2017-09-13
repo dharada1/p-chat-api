67e20d9fa43869554615409a68e2b743890fb2df
Daichi Harada <haradadaichi715@gmail.com>
2017-09-14 01:07:50 +0900

    必要なカラムメモ

--- api/models.py
+++ api/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class DummyUser(models.Model):
     name = models.CharField(max_length=50)
     gender = models.IntegerField(default=0) #Mなら1 Fなら2
+    #age追加する
+    #image追加する(DB保存でいいのか？)
     job = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     deleted = models.BooleanField(default=False)
