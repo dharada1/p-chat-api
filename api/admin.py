dea92b9c24f8b4ebcbb1e178717b6365fef338d7
Daichi Harada <haradadaichi715@gmail.com>
2017-09-14 14:08:20 +0900

    アドミン画面でAPI用モデルをいじれるようにした

--- api/admin.py
+++ api/admin.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import DummyUser, Message
+
+admin.site.register(DummyUser)
+admin.site.register(Message)
