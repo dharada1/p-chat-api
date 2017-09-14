1e0fa2133af16ab998fd20252bb3555d2e46e6d1
Daichi Harada <haradadaichi715@gmail.com>
2017-09-14 16:59:23 +0900

    IDをprintする

--- api/models.py
+++ api/models.py
@@ -12,7 +12,7 @@ class DummyUser(models.Model):
     deleted = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return str(self.id)
 
 class Message(models.Model):
     user = models.ForeignKey(DummyUser, related_name='user', null=True)
