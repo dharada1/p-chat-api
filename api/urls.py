45704c60b900c4f38e2807495f6f6fab6ce81b0e
Daichi Harada <haradadaichi715@gmail.com>
2017-09-14 14:20:56 +0900

    ダミー用URL削除

--- api/urls.py
+++ api/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     url(r'^message/history/$', views.message_history, name='message_history'),
     # ex: /api/message/create/
     url(r'^message/create/$', views.message_create, name='message_create'),
-    #ダミー
-    # ex: /api/message/history_dummy/
-    #url(r'^message/history_dummy/$', views.message_history_dummy, name='message_history_dummy'),
     # ex: /api/user/1/
     url(r'^user/(?P<user_id>[0-9]+)/$', views.user_data, name='user_data'),
 ]
